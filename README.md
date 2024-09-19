fad2435e6fedc2d34bb3cba157a12f345bc6b5c9
Antonio Montes <comicland_app@outlook.com>
2024-09-19 08:33:09 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,18 @@
+//
+//  README.md
+//  TestScanner
+//
+//  Created by Antonio Montes on 9/19/24.
+//
+
+# TestScanner
+
+Simple app to scan a bar code.
+
+## Credits
+
+https://www.hackingwithswift.com/example-code/media/how-to-scan-a-barcode by Paul Hudson.
+
+## License
+
+MIT
---
+++ TestScanner/ScannerVC.swift
@@ -0,0 +1,142 @@
+//
+//  ScannerVC.swift
+//  TestScanner
+//
+//  Created by Antonio Montes on 9/18/24.
+//  Copyright © 2024 Unknown. All rights reserved.
+//
+// https://www.hackingwithswift.com/example-code/media/how-to-scan-a-barcode
+
+import AVFoundation
+import UIKit
+
+class ScannerVC: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+
+    var captureSession: AVCaptureSession!
+    var previewLayer: AVCaptureVideoPreviewLayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor.black
+        captureSession = AVCaptureSession()
+
+        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
+        let videoInput: AVCaptureDeviceInput
+
+        do {
+            videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
+        }
+        catch {
+            return
+        }
+
+        if (captureSession.canAddInput(videoInput)) {
+            captureSession.addInput(videoInput)
+        }
+        else {
+            failed()
+            return
+        }
+
+        let metadataOutput = AVCaptureMetadataOutput()
+
+        if (captureSession.canAddOutput(metadataOutput)) {
+            captureSession.addOutput(metadataOutput)
+
+            metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            metadataOutput.metadataObjectTypes = [.ean8, .ean13, .pdf417]
+        }
+        else {
+            failed()
+            return
+        }
+
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.frame = view.layer.bounds
+        previewLayer.videoGravity = .resizeAspectFill
+        view.layer.addSublayer(previewLayer)
+
+        DispatchQueue.global(qos: .background).async {
+            self.captureSession.startRunning()
+        }
+    }
+
+    func failed() {
+
+        let ac = UIAlertController(title: "Scanning not supported",
+                                   message: "Your device does not support scanning a code from an item. Please use a device with a camera.",
+                                   preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+        captureSession = nil
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        if (captureSession?.isRunning == false) {
+            DispatchQueue.global(qos: .background).async {
+                self.captureSession.startRunning()
+            }
+        }
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+
+        if (captureSession?.isRunning == true) {
+            captureSession.stopRunning()
+        }
+    }
+
+    func metadataOutput(_ output: AVCaptureMetadataOutput,
+                        didOutput metadataObjects: [AVMetadataObject],
+                        from connection: AVCaptureConnection) {
+
+        captureSession.stopRunning()
+
+        if let metadataObject = metadataObjects.first {
+            guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
+            guard let stringValue = readableObject.stringValue else { return }
+            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+            found(code: stringValue)
+        }
+
+        dismiss(animated: true)
+    }
+
+    fileprivate func found(code: String) {
+
+        print(code)
+        let vc = UIViewController()
+        vc.view.backgroundColor = .green
+
+        let testLabel = UILabel()
+        testLabel.translatesAutoresizingMaskIntoConstraints = false
+        testLabel.textColor = .systemBackground
+        testLabel.text = code
+
+        vc.view.addSubview(testLabel)
+        activateConstraints(view: vc.view, testLabel: testLabel)
+
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+
+    fileprivate func activateConstraints(view: UIView,
+                                         testLabel: UILabel) {
+
+        NSLayoutConstraint.activate([
+            testLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            testLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+        ])
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .portrait
+    }
+}
